01b7788bf89eaf047f962742b292385b2a96e0c1
Anna Tankielun <anna.tankielun.3@gmail.com>
2023-07-15 20:04:54 +0200

    add store, initialState; add subpages;

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
+import { Routes, Route } from "react-router-dom";
+import Home from './components/pages/Home';
+import Post from './components/pages/Post';
+import AddPost from "./components/pages/AddPost";
+import EditPost from "./components/pages/EditPost";
+import About from "./components/pages/About";
+import NoMatch from "./components/pages/NoMatch";
+
+
 const App = () => {
+  
   return (
-    <h1>Hello world</h1>
+    <main>
+      
+      
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/post/:id" element={<Post />} />
+          <Route path="/post/add" element={<AddPost/>}/>
+          <Route path="/post/edit/:id" element={<EditPost/>}/>
+          <Route path="/about" element={<About />} />
+          <Route path="*" element={<NoMatch />} />
+        </Routes>
+      
+    </main>    
   );
+  
 };
 
 export default App;
---
+++ src/components/pages/Home.js
@@ -0,0 +1,7 @@
+const Home = () => {
+    return (
+        <h2>HOME PAGE</h2>
+    );
+};
+
+export default Home;
