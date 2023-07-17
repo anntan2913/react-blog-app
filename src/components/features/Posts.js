ef3e56bd55323c7a0dc8cfd53b2f3d39dec30585
Anna Tankielun <anna.tankielun.3@gmail.com>
2023-07-17 20:39:27 +0200

    small code reorganization im component Posts

--- src/components/features/Posts.js
+++ src/components/features/Posts.js
@@ -11,34 +11,23 @@ const Posts = () => {
         <section> 
             <Row>  
                 {posts.map(post => (
-                        <Col sm='8' md='4'> 
-                            <Card className='my-2' key={post.id}>
-                                <Card.Body>
-                                    <Card.Title>{post.title}</Card.Title>
-                                    <Card.Text>
-                                        <strong>Author:</strong>{post.author}
-                                        <br/>
-                                        <strong>Published:</strong>{post.publishedDate}
-                                        <br/>
-                                        <br/>
-                                        {post.shortDescription}                                        
-                                    </Card.Text>
-                                    <Button variant="primary" as={Link} to={`/post/${post.id}`} >Read more</Button>
-                                </Card.Body>                                
-                            </Card>      
-                        
-                        
-                        
-                        </Col>              
-                        ))} 
-
-
-
-                <Col >
-                    
-                </Col>
-
-
+                    <Col sm='12' md='4'> 
+                        <Card className='my-2' key={post.id}>
+                            <Card.Body>
+                                <Card.Title>{post.title}</Card.Title>
+                                <Card.Text>
+                                    <strong>Author: </strong>{post.author}
+                                    <br/>
+                                    <strong>Published: </strong>{post.publishedDate}
+                                    <br/>
+                                    <br/>
+                                    {post.shortDescription}                                        
+                                </Card.Text>
+                                <Button variant="primary" as={Link} to={`/post/${post.id}`} >Read more</Button>
+                            </Card.Body>                                
+                        </Card>                    
+                    </Col>              
+                ))}
             </Row>           
         </section>
     );
