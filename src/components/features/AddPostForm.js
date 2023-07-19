a1328045ca48c3e7b213ab63d56556cded3b2103
Anna Tankielun <anna.tankielun.3@gmail.com>
2023-07-19 15:31:49 +0200

    add function: add post (add component AddPostForm)

---
+++ src/components/features/AddPostForm.js
@@ -0,0 +1,61 @@
+import { Form, Button } from "react-bootstrap";
+import { useDispatch } from "react-redux";
+import { useState } from "react";
+import { addPost } from "../../redux/postsRedux";
+import shortid from "shortid";
+import { useNavigate } from "react-router-dom";
+
+const AddPostForm = () => {
+
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    
+    const [title, setTitle] = useState ('');
+    const [author, setAuthor] = useState('');
+    const [publishedDate, setPublishedDate] = useState('');
+    const [shortDescription, setShortDescription] = useState('');
+    const [content, setContent] = useState('');
+    
+    const handleAddPost = e => {
+        e.preventDefault();
+        navigate('/');
+        dispatch(addPost({ id: shortid.generate(), title, author, publishedDate, shortDescription, content }));
+        setTitle ('');
+        setAuthor('');
+        setPublishedDate('');
+        setShortDescription('');
+        setContent('');       
+    };
+
+    return(
+        <form >
+            <div style={{ width: '50%' }}>
+                <Form.Group className="mb-4" >
+                    <Form.Label>Title</Form.Label>
+                    <Form.Control type="text" placeholder="Enter title" value={title} onChange={e => setTitle(e.target.value)}  />
+                </Form.Group>
+                <Form.Group className="mb-4">
+                    <Form.Label>Author</Form.Label>
+                    <Form.Control type="text" placeholder="Enter author" value={author} onChange={e => setAuthor(e.target.value)}  />
+                </Form.Group>
+                <Form.Group className="mb-4">
+                    <Form.Label>Published</Form.Label>
+                    <Form.Control type="date" value={publishedDate} onChange={e => setPublishedDate(e.target.value)}  />
+                </Form.Group>
+            </div>
+            <div style={{ width: '100%' }}>
+                <Form.Group className="mb-4">
+                <Form.Label>Short description</Form.Label>
+                <Form.Control as="textarea" rows={3} placeholder="Leave a comment here" value={shortDescription} onChange={e => setShortDescription(e.target.value)}  />                
+                </Form.Group>
+                <Form.Group className="mb-4">
+                <Form.Label>Main content</Form.Label>
+                <Form.Control as="textarea" rows={9} placeholder="Leave a comment here" value={content} onChange={e => setContent(e.target.value)}  />
+                </Form.Group>                
+            </div>
+            <Button variant="primary" type="submit" onClick={handleAddPost} className="mb-4">Add Post</Button>            
+        </form>
+    );
+};
+
+export default AddPostForm;
--- src/components/pages/AddPost.js
+++ src/components/pages/AddPost.js
@@ -1,6 +1,11 @@
+import AddPostForm from "../features/AddPostForm";
+
 const AddPost = () => {
     return (
+        <div className="mx-auto" style={{ maxWidth: "900px" }}>
         <h2>Add Post</h2>
+        <AddPostForm/>
+        </div>
     );
 };
 
